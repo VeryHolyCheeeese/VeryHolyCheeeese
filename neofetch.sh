b6453e756ecd41f43bc3bf6fb30a20b2f08cc69a
DarkCheese_ <79263404+VeryHolyCheeeese@users.noreply.github.com>
2022-01-18 17:57:13 +0800

    Update neofetch.sh

--- neofetch.sh
+++ neofetch.sh
@@ -1,4 +1,4 @@
 neofetch
 Echo "You should look before you copy and paste something from the internet"
-#Some people can make it so that it look slike you copied for example "apt update" but instead you copied something like <script src="https://raw.githubusercontent.com/VeryHolyCheeeese/VeryHolyCheeeese/main/ree.js">
+#Some people can make it so that it look slike you copied for example "apt update" but instead you copied something like <script src="https://raw.githubusercontent.com/VeryHolyCheeeese/VeryHolyCheeeese/main/neofetch.js">
 #So i made one that just does neofetch
